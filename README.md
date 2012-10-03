e39bf19da672056096ad9635164f7b78cd533edb
tomaszdurka <tomasz@durka.pl>
2012-10-03 18:10:27 +0200

    Make logDir options, outputs to std out if not set Add `sockjs-url` option for SockJS `sockjs_url` option Improve onmessage error logging

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
- Redis.io
+socket-redis
 =========
 
-Script collects events from Redis server and distributes it to socket.io workers. Workload is split among all cpus.
-All socket.io workers listen on the same port using Cluster.
+Script collects events from Redis server and distributes it to SockJS worker.
+Server/worker is separation done to be prepared for workload distrubution over several workers.
 
 Arguments
 ---------
 
 `--redis-hosts` Specify comma separated hosts of redis servers. Master script will subscribe to all of them and distribute incoming events to workers. Defaults to `localhost`.
 
-`--socket-port` Port which socket.io workers will listen on. Defaults to `8090`.
+`--socket-port` Port which SockJS worker will listen on. Defaults to `8090`.
 
-`--log-dir` Directory where log(s) should be stored. Script will try to create directory if not exists. Will fail if no permission. Defaults to `/var/log/socket-redis`.
+`--log-dir` Directory where log(s) should be stored. Script will try to create directory if not exists. Will fail if no permission. Defaults to `null` which means it will output to stdout.
+
+`--sockjs-url` SockJS client url. Defaults to `http://cdn.sockjs.org/sockjs-0.3.min.js`. Read more at https://github.com/sockjs/sockjs-client
--- bin/socket-redis.js
+++ bin/socket-redis.js
@@ -2,10 +2,11 @@
 var socketRedis = require('../socket-redis.js'),
 	fs = require('fs'),
 	path = require('path'),
-	argv = require('optimist').default('redis-hosts', 'localhost').default('socket-port', '8090').default('log-dir', '/var/log/socket-redis/').argv;
+	argv = require('optimist').default('redis-hosts', 'localhost').default('socket-port', '8090').default('log-dir', null).default('sockjs-url', null).argv;
 
 var redisHosts = argv['redis-hosts'].split(','),
 	socketPort = argv['socket-port'],
+	sockjsUrl = argv['sockjs-url'],
 	logDir = argv['log-dir'];
 
 fs.mkdirRecursive = function(directory) {
@@ -16,14 +17,16 @@ fs.mkdirRecursive = function(directory) {
 	}
 };
 
-fs.mkdirRecursive(logDir);
-var log = fs.createWriteStream(logDir + '/general.log', {'flags': 'a+', 'encoding': 'utf8', 'mode': 0644});
-process.__defineGetter__('stdout', function() { return log; });
-process.__defineGetter__('stderr', function() { return log; });
-process.on('uncaughtException', function(e) { process.stderr.write('Uncaught exception: ' + e + '\n'); });
+if (logDir) {
+	fs.mkdirRecursive(logDir);
+	var log = fs.createWriteStream(logDir + '/general.log', {'flags': 'a+', 'encoding': 'utf8', 'mode': 0644});
+	process.__defineGetter__('stdout', function() { return log; });
+	process.__defineGetter__('stderr', function() { return log; });
+	process.on('uncaughtException', function(e) { process.stderr.write('Uncaught exception: ' + e + '\n'); });
+}
 
 var publisher = new socketRedis.Server(redisHosts);
-var worker = new socketRedis.Worker(socketPort);
+var worker = new socketRedis.Worker(socketPort, sockjsUrl);
 publisher.onMessage = function(channel, message) {
 	worker.publish(channel, message);
 };
