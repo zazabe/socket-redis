fc840fde88b7a678f82e011ef10a99a5741f4a58
Michel Meyer <michel.meyer@cargomedia.ch>
2017-07-25 20:27:45 +0200

    Don't push to hub.docker.com

--- script/docker-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-BUILD="${TRAVIS_REPO_SLUG}:build"
-TARGET="${1:-${TRAVIS_REPO_SLUG}:${TRAVIS_TAG}}"
-
-docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker build . -t "${BUILD}"
-docker tag "${BUILD}" "${TARGET}"
-docker push "${TARGET}"
